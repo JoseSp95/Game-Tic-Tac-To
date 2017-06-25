6b230be5f4bf82ee0cec47a03f418c451e7147f9
Jose Suarez <jose_95sp@hotmail.com>
2017-06-24 20:28:11 -0500

    se agregaron gran parte de la interfaz y las casillas blancas

--- app/src/main/java/com/example/josesp/gameofnoughtsandcrosses/MainActivity.java
+++ app/src/main/java/com/example/josesp/gameofnoughtsandcrosses/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.josesp.gameofnoughtsandcrosses;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
